cf90c0bcf43a86007873afea58ccf49882029b07
shaygyawali <shaygyawali@gmail.com>
2018-06-19 19:23:15 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -1,17 +1,66 @@
-def first_steps
-  # Write a loop that outputs "Right foot back", "Left foot back",
-  # "Right foot back", and "Stop", with sleep after each
-end
 
-def a_few_more_steps
-  # Write a loop that outputs the first two sets of steps in the Two-Step
-end
+  def first_steps
+    loop do
+      puts "Right foot back"
+      sleep(0.5)
+      puts "Left foot back"
+      sleep(0.5)
+      puts "Right foot back"
+      sleep(0.5)
+      puts "stop"
+      sleep(1)
+      puts "Turn"
+      break
+    end
+  end
+  
+  first_steps
+  
+  def a_few_more_steps
+    loop do
+      puts "Right foot back"
+      sleep(0.5)
+      puts "Left foot back"
+      sleep(0.5)
+      puts "Right foot back"
+      sleep(0.5)
+      puts "stop"
+      sleep(1)
+      puts "right foot steps right"
+      sleep (0.5)
+      puts "Left foot crosses over right"
+      sleep(0.5)
+      puts "Right foot steps right"
+      sleep(0.5)
+      puts "Turn"
+      sleep(1)
+    end
+  end
+  
+  a_few_more_steps
+  
+  
+  def how_many_steps?
+    steps = 0 
+    puts steps
+    steps += 1
+    if steps % 2 == 0 
+      puts "Left"
+    else 
+      puts "Right"
+    end 
+  end
+  
+  how_many_steps?
+  
+  def break_dance
+    # Write a solution that uses the same code as how_many_steps?, but breaks the
+    # loop if steps is equal to 6
+  end
+  
+  
+  
+  
+  
 
-def how_many_steps?
-  # Write a loop that fulfills all criteria from Part 3 of the README.md
-end
 
-def break_dance
-  # Write a solution that uses the same code as how_many_steps?, but breaks the
-  # loop if steps is equal to 6
-end
